b3202d177a2d3e01c9b0d51f014f119a6ed3d2a1
Khoa Tran <khoa@autonomous.nyc>
2018-11-20 17:08:18 +0700

    Fix token refresh

--- src/coin_auth/urls.py
+++ src/coin_auth/urls.py
@@ -3,8 +3,8 @@ from rest_framework_simplejwt.views import TokenObtainPairView, TokenVerifyView
 
 patterns = ([
     path('', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('refresh', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('verify', TokenVerifyView.as_view(), name='token_verify'),
+    path('refresh/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('verify/', TokenVerifyView.as_view(), name='token_verify'),
 ], 'token')
 
 urlpatterns = [
