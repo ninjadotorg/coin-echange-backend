ed3b8eb56755432edbf7c62569dc9bd5b4c115f8
Khoa Tran <khoa@autonomous.nyc>
2018-11-20 17:35:06 +0700

    Update refresh token

--- src/coin_auth/urls.py
+++ src/coin_auth/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path, include
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenVerifyView
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenVerifyView, TokenRefreshView
 
 patterns = ([
     path('', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('refresh/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('verify/', TokenVerifyView.as_view(), name='token_verify'),
 ], 'token')
 
