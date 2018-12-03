9a31b156956d76be5e9c690f7d848fe9d3302c40
Khoa Tran <khoa@autonomous.nyc>
2018-12-03 18:30:37 +0700

    File upload Fix phone verification

--- src/integration/google_storage.py
+++ src/integration/google_storage.py
@@ -1,5 +1,4 @@
 import os
-import sys
 
 from django.conf import settings
 from google.cloud import storage
@@ -8,15 +7,23 @@ from google.cloud.storage import Blob
 from common.decorators import raise_api_exception
 from integration.exceptions import ExternalAPIException
 
-if 'test' not in sys.argv:
-    client = storage.Client.from_service_account_json(
-        os.path.normpath(os.path.join(settings.DJANGO_ROOT, 'coin-exchange-storage.json')))
-    bucket = client.get_bucket(settings.STORAGE_BUCKET)
+
+bucket = None
 
 
 @raise_api_exception(ExternalAPIException)
 def upload_file(full_file_name: str, file):
-    blob = Blob(full_file_name, bucket)
+
+    blob = Blob(full_file_name, get_bucket())
     blob.upload_from_file(file)
 
     return blob
+
+
+def get_bucket():
+    global bucket
+    if not bucket:
+        client = storage.Client.from_service_account_json(
+            os.path.normpath(os.path.join(settings.DJANGO_ROOT, 'coin-exchange-storage.json')))
+        bucket = client.get_bucket(settings.STORAGE_BUCKET)
+    return bucket
